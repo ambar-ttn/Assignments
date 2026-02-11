1543f628c7fca40c0b0d015769f1e690b4ae9df6
Ambar <ambargoyal3@gmial.com>
2026-02-11 15:13:15 +0530

    Initial React setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { Card } from './Card';
+import Class from './Class';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>Hello World</h1>
+      
+      {/* <h1>Learning React with ppl !!!</h1>
+      <Card name="Ambar" lastName="Goel" />
+      <Class /> */}
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/Card.js
@@ -0,0 +1,5 @@
+export function Card(props){
+       return(
+            <h1>Functional Component</h1>
+       )
+}
